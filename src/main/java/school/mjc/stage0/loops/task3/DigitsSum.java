19fef2b976445c10b228bbceb7d7c5430a020f3c
volbog <volbog93@gmail.com>
2023-05-15 22:15:29 +0200

    finish all tasks

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        int sum = 0;
+        String numStr = Math.abs(t) + "";
+        for (int i = 0; i < numStr.length(); i++) {
+            int digit = Character.getNumericValue(numStr.charAt(i));
+            sum += digit;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int greatest = 1;
+
+        for (int i = 1; i <= (Math.max(second, first)); i++) {
+            if(first % i == 0 && second % i == 0) {
+                greatest = i;
+            }
+        }
+        System.out.println(greatest);
     }
 }
